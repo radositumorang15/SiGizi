5dcf2a7d067da27aa9b13d17689a6366b9ba53df
Satrio Wicaksono Nugroho <82352385+satriown@users.noreply.github.com>
2024-06-14 12:25:13 +0700

    fitur nutrisi
    - tambah folder nutrisi beserta desain kasaran
    - isi fiturnya ada pada string.xml

---
+++ app/src/main/java/com/example/sigizi/view/nutrisi/DetailNutrisiActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sigizi.view.nutrisi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sigizi.R
+
+class DetailNutrisiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_nutrisi)
+    }
+}
---
+++ app/src/main/java/com/example/sigizi/view/nutrisi/NutrisiActivity.kt
@@ -0,0 +1,14 @@
+package com.example.sigizi.view.nutrisi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sigizi.R
+
+class NutrisiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nutrisi)
+
+
+    }
+}
